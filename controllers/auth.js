28c85fe831ee62d959d63caf660d5f3307937eab
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-07-07 12:46:06 +0300

    connects successfuly to the database

---
+++ controllers/auth.js
@@ -0,0 +1,26 @@
+import bcrypt from 'bcryptjs'
+import User from '../models/Users.js'
+import jwt from 'jsonwebtoken'
+
+export const register = async (req, res, next) => {
+	
+}
+
+export const login = async (req, res, next) => {
+	
+}
+
+// logout user
+export const logout = async (req, res, next) => {
+	
+}
+
+// Change Password
+export const changePassword = async (req, res, next) => {
+	
+}
+
+// Refresh Access Token
+export const refreshAccessToken = async (req, res, next) => {
+	
+}
---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+// import helmet from "helmet"
+import { connectDb } from './config/db.js'
+
+import authRoutes from './routes/auth.js'
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+const allowedOrigins = ['https://alx-tour-destination.vercel.app', 'http://localhost:5173']
+app.use(
+	cors({
+		origin: function (origin, callback) {
+			// allow requests with no origin (like mobile apps, curl, Postman)
+			if (!origin) return callback(null, true)
+			if (allowedOrigins.includes(origin)) {
+				return callback(null, true)
+			} else {
+				return callback(new Error('CORS not allowed from this origin ->: ' + origin))
+			}
+		},
+		credentials: true,
+	})
+)
+
+connectDb()
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+app.use('/api/v1/auth', authRoutes)
+
+const PORT = process.env.PORT || process.env.npm_package_config_port
+
+app.listen(PORT, () => {
+	console.log(`Success 💯! Servers running on port: ${PORT} 👍👍`)
+})
