6a403d31bdb5852a34aed0dd72d226e56b7f89b0
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-07-15 11:36:33 +0300

    minor adjustments

--- models/Users.js
+++ models/Users.js
@@ -13,12 +13,12 @@ const UserSchema = new mongoose.Schema(
 			enum: ['frelancer', 'employer', 'admin'],
 			default: 'frelancer',
 		},
-		reviews: [
-		    {
-		      type: mongoose.Schema.Types.ObjectId,
-		      ref: 'Review',
-		    },
-		],
+		// reviews: [
+		//     {
+		//       type: mongoose.Schema.Types.ObjectId,
+		//       ref: 'Review',
+		//     },
+		// ],
 		averageRating: {
 		    type: Number,
 		    default: 0,
