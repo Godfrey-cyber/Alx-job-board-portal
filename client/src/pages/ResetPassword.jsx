cac13918d80e071370a521910164edbc3a5907f6
Godfrey-cyber <godfreyndiritu26@gmail.com>
2025-07-18 08:32:23 +0300

    made minir adjustment changes

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { modules } from "../utilities/utilities.js"
 
 const ResetPassword = () => {
-	return (
-		<div className="flex flex-col bg-white h-screen justify-center items-center bg-red-400 w-full">
-			Reset Password
-		</div>
+	const [description, setDescription] = useState('');
+  const [responsibilities, setResponsibilities] = useState('');
+  const [qualifications, setQualifications] = useState('');
+  const [skills, setSkills] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const jobData = {
+      description,
+      responsibilities,
+      qualifications,
+      skills,
+      // other fields like title, location...
+    };
+    console.log(jobData);
+    // send to backend
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {/* Title, Location, etc. */}
+
+      <label>Job Description</label>
+      <ReactQuill value={description} onChange={setDescription} />
+
+      <label>Responsibilities</label>
+      <ReactQuill value={responsibilities} onChange={setResponsibilities} />
+
+      <label>Qualifications</label>
+      <ReactQuill value={qualifications} onChange={setQualifications} />
+
+      <label>Skills</label>
+      <ReactQuill value={skills} onChange={setSkills} />
+
+      <button type="submit">Submit Job</button>
+    </form>
 	)
 }
 
